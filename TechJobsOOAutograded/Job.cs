922fd7a10e62ad756f06d5ec123299afe2551f13
ck <clairekrug17@gmail.com>
2022-09-08 18:44:11 -0500

    Part 2 second commit

--- TechJobsOOAutograded/Job.cs
+++ TechJobsOOAutograded/Job.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Data;
+using System.Xml.Linq;
+
 namespace TechJobsOO
 {
     public class TechJob
     {
-        /*
+        
         public int Id { get; }
         private static int nextId = 1;
 
@@ -14,9 +17,35 @@ namespace TechJobsOO
         public CoreCompetency JobCoreCompetency { get; set; }
 
         // TODO: Add the two necessary constructors.
+        public TechJob()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public TechJob(string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency) : this()
+        {
+            Name = name;
+            EmployerName = employerName;
+            EmployerLocation = employerLocation;
+            JobType = jobType;
+            JobCoreCompetency = jobCoreCompetency;
+
+        }
 
         // TODO: Generate Equals() and GetHashCode() methods.
 
-        */
+        public override bool Equals(object obj)
+        {
+            return obj is TechJob techJob &&
+                   Id == techJob.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        } 
+
+
     }
 }
